396bbe7539c6b5e435e61eebdca9aa53927b50e8
HerbethMilhome <h2milhome@gmail.com>
2024-02-04 16:20:57 -0300

    init

---
+++ src/main/java/br/com/csmastery/CsmasteryApplication.java
@@ -0,0 +1,13 @@
+package br.com.csmastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsmasteryApplication.class, args);
+	}
+
+}
