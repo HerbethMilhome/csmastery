e5fbab398a70148d7b833e42354341ac3aac4680
HerbethMilhome <h2milhome@gmail.com>
2024-02-04 19:30:48 -0300

    add controller aluno

---
+++ src/main/java/br/com/csmastery/aluno/controller/AlunoController.java
@@ -0,0 +1,16 @@
+package br.com.csmastery.aluno.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/aluno")
+public class AlunoController {
+
+    @GetMapping
+    public ResponseEntity getAllAlunos() {
+        return ResponseEntity.ok("deu certo");
+    }
+}
